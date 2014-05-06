6f8435b38f044636acf672731de12903a18f2e60
Matt Tytel <mtytel@leapmotion.com>
2014-05-05 17:38:21 -0700

    Upgraded to new Skeletal Unity Assets.

--- Assets/LeapMotion/Skeletal/Scripts/Hands/PolyHand.cs
+++ Assets/LeapMotion/Skeletal/Scripts/Hands/PolyHand.cs
@@ -1,35 +1,23 @@
 ﻿/******************************************************************************\
 * Copyright (C) Leap Motion, Inc. 2011-2014.                                   *
-* Leap Motion proprietary and  confidential.  Not for distribution.            *
-* Use subject to the terms of the Leap Motion SDK Agreement available at       *
-* https://developer.leapmotion.com/sdk_agreement, or another agreement between *
-* Leap Motion and you, your company or other organization.                     *
-* Author: Matt Tytel
+* Leap Motion proprietary. Licensed under Apache 2.0                           *
+* Available at http://www.apache.org/licenses/LICENSE-2.0.html                 *
 \******************************************************************************/
 
 using UnityEngine;
 using System.Collections;
 using Leap;
 
-// A deforming very low poly count hand.
+// A deforming, very low poly count hand.
 public class PolyHand : HandModel {
-
-  void Start() {
-  }
-
   public override void InitHand(Transform deviceTransform) {
-    Hand leap_hand = GetLeapHand();
-    Vector3 palm_normal = deviceTransform.TransformDirection(leap_hand.PalmNormal.ToUnity());
-    Vector3 palm_direction = deviceTransform.TransformDirection(leap_hand.Direction.ToUnity());
-
-    for (int f = 0; f < fingers.Length; ++f) {
-      if (fingers[f] != null)
-        fingers[f].InitFinger(deviceTransform, palm_normal, palm_direction);
-    }
+    UpdateHand(deviceTransform);
   }
 
   public override void UpdateHand(Transform deviceTransform) {
-    InitHand(deviceTransform);
+    for (int f = 0; f < fingers.Length; ++f) {
+      if (fingers[f] != null)
+        fingers[f].InitFinger(deviceTransform);
+    }
   }
-
 }
